58091fc455fa523b1c1a1c5de84878081015c3a9
Tim Logeman <til@move-elevator.de>
2019-02-27 08:25:38 +0100

    set numberofLines to 2 and change btn text style to red

--- src/components/raised-text-button/index.js
+++ src/components/raised-text-button/index.js
@@ -38,8 +38,8 @@ export default class RaisedTextButton extends PureComponent {
     return (
       <RaisedButton rippleColor={titleColor} shadeColor={titleColor} {...props}>
         <Text
-          style={[styles.title, titleStyle, titleStyleOverrides]}
-          numberOfLines={1}
+          style={[styles.title, titleStyle, titleStyleOverrides, color: 'red']}
+          numberOfLines={2}
         >
           {String(title).toUpperCase()}
         </Text>
